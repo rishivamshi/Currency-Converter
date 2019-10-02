a604333640556623bf794ad42e3db4d82c35d950
bharath <v.ramakoteswararao2015@vit.ac.in>
2019-10-02 22:51:22 +0530

    Modularized the code by creating a main function

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Created on Sat Nov 25 20:08:51 2017
 
 from tkinter import *
 import time
-from currency_converter import CurrencyConverter
+from currency_converter import CurrencyConverter as CC
 import pandas as pd
 import csv
 from tables import createStandardTable as cst
@@ -66,7 +66,7 @@ def Ref():
    
     global coi
     coi = float(cInput.get())
-    c = CurrencyConverter()
+    c = CC()
     value = c.convert(coi,cof,cob)
     Cost.set(value)
     
@@ -112,80 +112,82 @@ def Reset():
     Cost.set("")
     
   
-txtDisplay=Entry(f2,font=('arial',20,'bold'),textvariable=cInput,bd=30,insertwidth=4,bg="alice blue",justify='right')
-txtDisplay.grid(columnspan=4)
-btn7=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
-            text="7",bg="gray50",command=lambda: btnClick(7)).grid(row=2,column=0)
-btn8=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
-            text="8",bg="gray50",command=lambda: btnClick(8)).grid(row=2,column=1)
-btn9=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
-            text="9",bg="gray50",command=lambda: btnClick(9)).grid(row=2,column=2)
+def main():
+    txtDisplay=Entry(f2,font=('arial',20,'bold'),textvariable=cInput,bd=30,insertwidth=4,bg="alice blue",justify='right')
+    txtDisplay.grid(columnspan=4)
+    btn7=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
+                text="7",bg="gray50",command=lambda: btnClick(7)).grid(row=2,column=0)
+    btn8=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
+                text="8",bg="gray50",command=lambda: btnClick(8)).grid(row=2,column=1)
+    btn9=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
+                text="9",bg="gray50",command=lambda: btnClick(9)).grid(row=2,column=2)
 
 
 
-btn4=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
-            text="4",bg="gray50",command=lambda: btnClick(4)).grid(row=3,column=0)
-btn5=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
-            text="5",bg="gray50",command=lambda: btnClick(5)).grid(row=3,column=1)
-btn6=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
-            text="6",bg="gray50",command=lambda: btnClick(6)).grid(row=3,column=2)
-
-
-
-
-btn1=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
-            text="1",bg="gray50",command=lambda: btnClick(1)).grid(row=4,column=0)
-btn2=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
-            text="2",bg="gray50",command=lambda: btnClick(2)).grid(row=4,column=1)
-btn3=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
-            text="3",bg="gray50",command=lambda: btnClick(3)).grid(row=4,column=2)
+    btn4=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
+                text="4",bg="gray50",command=lambda: btnClick(4)).grid(row=3,column=0)
+    btn5=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
+                text="5",bg="gray50",command=lambda: btnClick(5)).grid(row=3,column=1)
+    btn6=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
+                text="6",bg="gray50",command=lambda: btnClick(6)).grid(row=3,column=2)
 
 
 
 
-btn0=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
-            text="0",bg="gray50",command=lambda: btnClick(0)).grid(row=5,column=0)
-btnClear=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
-            text="C",bg="dark orange",command= btnClearDisplay).grid(row=5,column=1)
-btnEquals=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
-            text="=",bg="dark orange",command= Ref).grid(row=5,column=2)
+    btn1=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
+                text="1",bg="gray50",command=lambda: btnClick(1)).grid(row=4,column=0)
+    btn2=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
+                text="2",bg="gray50",command=lambda: btnClick(2)).grid(row=4,column=1)
+    btn3=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
+                text="3",bg="gray50",command=lambda: btnClick(3)).grid(row=4,column=2)
 
 
-cFrom=StringVar()
-cTo=StringVar()
-
-cFrom.set("USD")
-cTo.set("INR")
-cInput.set(0.0)
-
-Cost=StringVar()
 
 
-lblcFrom=Label(f1,font=('arial',16,'bold'),text="FROM",bd=16,anchor='w')
-lblcFrom.grid(row=1,column=0)
-txtcFrom=Entry(f1,font=('arial',16,'bold'),textvariable=cFrom,bd=10,insertwidth=4,bg="alice blue",justify="right")
-txtcFrom.grid(row=1,column=1)
-
-lblcTo=Label(f1,font=('arial',16,'bold'),text="TO",bd=16,anchor='w')
-lblcTo.grid(row=2,column=0)
-txtcTo=Entry(f1,font=('arial',16,'bold'),textvariable=cTo,bd=10,insertwidth=4,bg="alice blue",justify="right")
-txtcTo.grid(row=2,column=1)
-
-lblCost=Label(f1,font=('arial',16,'bold'),text="------------",bd=16,anchor='w')
-lblCost.grid(row=3,column=0)
-lblCost=Label(f1,font=('arial',16,'bold'),text="-------------------------------------",bd=16,anchor='w')
-lblCost.grid(row=3,column=1)
-
-lblCost=Label(f1,font=('arial',16,'bold'),text="Conversion",bd=16,anchor='w')
-lblCost.grid(row=4,column=0)
-txtCost=Entry(f1,font=('arial',16,'bold'),textvariable=Cost,bd=10,insertwidth=4,bg="#ffffff",justify="right")
-txtCost.grid(row=4,column=1)
-
-btnTotal=Button(f1,padx=16,pady=16,bd=8,fg="white",font=('arial',16,'bold'),width=10,text="Total",bg="dark orange",command=Ref).grid(row=1,column=3)
-btnReset=Button(f1,padx=16,pady=16,bd=8,fg="white",font=('arial',16,'bold'),width=10,text="Reset",bg="firebrick",command=Reset).grid(row=2,column=3)
-btnExit=Button(f1,padx=16,pady=16,bd=8,fg="black",font=('arial',16,'bold'),width=10,text="Quit",bg="powder blue",command=qExit).grid(row=4,column=3)
+    btn0=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
+                text="0",bg="gray50",command=lambda: btnClick(0)).grid(row=5,column=0)
+    btnClear=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
+                text="C",bg="dark orange",command= btnClearDisplay).grid(row=5,column=1)
+    btnEquals=Button(f2,padx=16,pady=16,bd=8,fg="white",font=('arial',20,'bold'),
+                text="=",bg="dark orange",command= Ref).grid(row=5,column=2)
 
 
-root.mainloop()
+    cFrom=StringVar()
+    cTo=StringVar()
+
+    cFrom.set("USD")
+    cTo.set("INR")
+    cInput.set(0.0)
+
+    Cost=StringVar()
 
 
+    lblcFrom=Label(f1,font=('arial',16,'bold'),text="FROM",bd=16,anchor='w')
+    lblcFrom.grid(row=1,column=0)
+    txtcFrom=Entry(f1,font=('arial',16,'bold'),textvariable=cFrom,bd=10,insertwidth=4,bg="alice blue",justify="right")
+    txtcFrom.grid(row=1,column=1)
+
+    lblcTo=Label(f1,font=('arial',16,'bold'),text="TO",bd=16,anchor='w')
+    lblcTo.grid(row=2,column=0)
+    txtcTo=Entry(f1,font=('arial',16,'bold'),textvariable=cTo,bd=10,insertwidth=4,bg="alice blue",justify="right")
+    txtcTo.grid(row=2,column=1)
+
+    lblCost=Label(f1,font=('arial',16,'bold'),text="------------",bd=16,anchor='w')
+    lblCost.grid(row=3,column=0)
+    lblCost=Label(f1,font=('arial',16,'bold'),text="-------------------------------------",bd=16,anchor='w')
+    lblCost.grid(row=3,column=1)
+
+    lblCost=Label(f1,font=('arial',16,'bold'),text="Conversion",bd=16,anchor='w')
+    lblCost.grid(row=4,column=0)
+    txtCost=Entry(f1,font=('arial',16,'bold'),textvariable=Cost,bd=10,insertwidth=4,bg="#ffffff",justify="right")
+    txtCost.grid(row=4,column=1)
+
+    btnTotal=Button(f1,padx=16,pady=16,bd=8,fg="white",font=('arial',16,'bold'),width=10,text="Total",bg="dark orange",command=Ref).grid(row=1,column=3)
+    btnReset=Button(f1,padx=16,pady=16,bd=8,fg="white",font=('arial',16,'bold'),width=10,text="Reset",bg="firebrick",command=Reset).grid(row=2,column=3)
+    btnExit=Button(f1,padx=16,pady=16,bd=8,fg="black",font=('arial',16,'bold'),width=10,text="Quit",bg="powder blue",command=qExit).grid(row=4,column=3)
+
+
+    root.mainloop()
+
+
+main()
